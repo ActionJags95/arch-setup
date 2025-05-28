4f0e6128f3070cfa51d6102b1aae55677f691f3c
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-05-28 20:31:59 +0530

    changes

--- test.sh
+++
@@ -1,7 +0,0 @@
-for directory in */ ; do
-  echo $directory
-  # if [[ -f "$directory/packages.conf" ]]; then
-  #    source "$directory/packages.conf"
-  # fi
-  # source "$directory/install.sh"
-done
