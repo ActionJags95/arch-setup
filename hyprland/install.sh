a9b473fc46a313e4efd5681c1942eb7af50bf25d
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-10-26 08:13:32 +0530

    Package checking will not log the package not found error

--- hyprland/install.sh
+++ hyprland/install.sh
@@ -25,7 +25,7 @@ echo "Starting packages installation..."
 
 for package in "${PACKAGES[@]}" ; do
   pause_msg "Package: $package, initiating installation..."
-  if yay -Q "$package" > /dev/null ; then
+  if yay -Q "$package" 2> /dev/null ; then
     pause_msg "$package already installed, skipping installation."
   else
     yay -S --noconfirm "$package"
