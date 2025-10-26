49311f13f2438672d18ee4ec28baebb162356d1c
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-10-26 11:45:36 +0530

    Repurposed package install utility

--- packages/install.sh
+++ packages/install.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
-function greeter() {
-  echo "
-███████╗██╗   ██╗███████╗████████╗███████╗███╗   ███╗    ██╗   ██╗████████╗██╗██╗     ███████╗
-██╔════╝╚██╗ ██╔╝██╔════╝╚══██╔══╝██╔════╝████╗ ████║    ██║   ██║╚══██╔══╝██║██║     ██╔════╝
-███████╗ ╚████╔╝ ███████╗   ██║   █████╗  ██╔████╔██║    ██║   ██║   ██║   ██║██║     ███████╗
-╚════██║  ╚██╔╝  ╚════██║   ██║   ██╔══╝  ██║╚██╔╝██║    ██║   ██║   ██║   ██║██║     ╚════██║
-███████║   ██║   ███████║   ██║   ███████╗██║ ╚═╝ ██║    ╚██████╔╝   ██║   ██║███████╗███████║
-╚══════╝   ╚═╝   ╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝     ╚═════╝    ╚═╝   ╚═╝╚══════╝╚══════╝
-
-  *System Utilites Installation*
-  "
-}
-clear
-greeter
-sleep 2
-
 function pause_msg() {
   echo "$1"
   sleep 1
 }
 
-echo "Starting packages installation..."
+CWD=$(pwd)
 
-for package in "${PACKAGES[@]}" ; do
-  pause_msg "Package: $package, initiating installation..."
-  if yay -Q "$package" 2> /dev/null ; then
-    pause_msg "$package already installed, skipping installation."
-  else
-    yay -S --noconfirm "$package"
-  fi
+for PACKAGE_FILE in "$CWD"/*.conf ; do
+  source "$PACKAGE_FILE"
+  clear
+  echo "$MASCOT"
+  sleep 2
+  echo "Starting packages installation..."
+
+  for package in "${PACKAGES[@]}" ; do
+    pause_msg "Package: $package, initiating installation..."
+    if yay -Q "$package" 2> /dev/null ; then
+      pause_msg "$package already installed, skipping installation."
+    else
+      yay -S --noconfirm "$package"
+    fi
+  done
 done
