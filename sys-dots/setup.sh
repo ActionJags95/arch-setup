dd140f6bfbec05064dacff075ebc6dfb8eaa34f7
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-10-26 17:03:16 +0530

    Rectified some errors

--- sys-dots/setup.sh
+++ sys-dots/setup.sh
@@ -6,16 +6,19 @@ echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"' | sudo tee -a /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # Copying dracut config
-if [ -d "$SYS_DOTS/dracut.conf.d" ] ; then
-  sudo cp -r "$SYS_DOTS/dracut.conf.d" "/etc/"
+if [ -d "$SYSDOTS_DIR/dracut.conf.d" ] ; then
+  sudo cp -r "$SYSDOTS_DIR/dracut.conf.d" "/etc/"
 fi
 
 # Copying and setting the plymouth theme
-echo "$SYS_DOTS"
-if [ -d "/usr/share/plymouth/" ] && [ -d "$SYS_DOTS/catppuccin-macchiato" ] ; then
+echo "$SYSDOTS_DIR"
+if [ -d "/usr/share/plymouth/" ] && [ -d "$SYSDOTS_DIR/catppuccin-macchiato" ] ; then
   sudo cp -r "$SYS_DOTS/catppuccin-macchiato" "/usr/share/plymouth/themes/"
   sudo plymouth-set-default-theme -R catppuccin-macchiato
 fi
 
 # Building init-system
+if [ -f "$SYSDOTS_DIR/mkinitcpio.conf" ] ; then
+  sudo cp "$SYSDOTS_DIR/mkinitcpio.conf" "/etc/mkinitcpio.conf"
+fi
 sudo mkinitcpio -P
